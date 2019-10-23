8dbe6de6a9c14a717ed0ea087f74fcea0e56d653
Corteggiani <n.corteggiani@gmail.com>
2019-10-23 12:24:01 +0200

    improve error handling

--- avatar2/protocols/inception.py
+++ avatar2/protocols/inception.py
@@ -81,9 +81,11 @@ class InceptionProtocol(object):
             self.log.error('Failed to connect to Inception-debugger')
             raise ConnectionRefusedError("Inception-debugger is not connected")
 
-        #self._device.reset();
-
-        #self._device.set_configuration()
+        try:
+            self._device.set_configuration()
+            self._device.reset()
+        except usb.core.USBError as e:
+            raise ConnectionRefusedError("Could not set configuration: %s" % str(e))
 
         # # get an endpoint instance
         # cfg = self._device.get_active_configuration()
